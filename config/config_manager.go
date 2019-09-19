feab498a4a277bbe66c5889f8666a843cfa5abf5
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-19 23:27:25 +0200

    Initial version of config manager

--- config/config_manager.go
+++ config/config_manager.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/xdevices/utilities/config"
+)
+
+type registerConfig struct {
+	config.Manager
+	dbPath string
+}
+
+var instance *registerConfig
+
+func Config() *registerConfig {
+	if instance == nil {
+		instance = new(registerConfig)
+		instance.Init()
+	}
+	return instance
+}
+
+func (c *registerConfig) registerConfigInit() {
+	if dbPath, err := os.LookupEnv("DB_PATH"); !err {
+		panic(fmt.Sprintf("set DB_PATH and try again"))
+	} else {
+		c.dbPath = dbPath
+	}
+}
+
+func (c *registerConfig) DBPath() string {
+	return c.dbPath
+}
