d2aa03eb04ed701ba1d2e00f1f685a27b994b335
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-01 08:04:21 +0200

    Initial version of getall sensors handler with unit tests

---
+++ handlers/sensor/sensor_getall.go
@@ -0,0 +1,20 @@
+package sensor
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/xdevices/register/services/sensor"
+)
+
+func HandleGetSensors(c echo.Context) error {
+
+	dtos, err := sensor.Service.GetAll()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	if len(dtos) == 0 {
+		return c.JSON(http.StatusNoContent, nil)
+	}
+	return c.JSON(http.StatusOK, dtos)
+}
---
+++ handlers/sensor/sensor_getall_test.go
@@ -0,0 +1,96 @@
+package sensor
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/xdevices/utilities/db"
+	"github.com/xdevices/utilities/symbols"
+
+	"github.com/labstack/echo"
+
+	"github.com/xdevices/register/dbprovider"
+	"github.com/xdevices/register/dto"
+	"github.com/xdevices/register/services/sensor"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+// suite struct
+type HandleSensorsGetAllSuite struct {
+	suite.Suite
+}
+
+// init suite
+func TestHandleSensorsGetAllSuite(t *testing.T) {
+	suite.Run(t, new(HandleSensorsGetAllSuite))
+}
+
+// setup test func
+func (h *HandleSensorsGetAllSuite) SetupTest() {
+	dbprovider.EnvironmentPreparations()
+	sensor.Init()
+}
+
+// failure test - no content
+func (h *HandleSensorsGetAllSuite) TestFailure_getall_no_content() {
+
+	// Arrange.
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Act.
+
+	_ = HandleGetSensors(c)
+
+	// Assert.
+
+	assert.Equal(h.T(), http.StatusNoContent, rec.Code)
+}
+
+// success test - content available
+func (h *HandleSensorsGetAllSuite) TestSuccess_getall_content_available() {
+
+	// Arrange.
+
+	cleaner := db.DeleteCreatedEntities(dbprovider.Mgr.GetDb())
+	defer cleaner()
+
+	var attributes []dto.SensorAttributeDTO
+	attributes = append(attributes, dto.SensorAttributeDTO{Symbol: symbols.Active.String(), Value: "true"})
+
+	registerDTO := dto.SensorRegisterDTO{
+		Name:       "Dummy sensor",
+		Type:       "DUMMY_TYPE",
+		Uuid:       "81750491-88dd-410e-b53f-1666786cd721",
+		Attributes: attributes,
+	}
+	_, _ = sensor.Service.Save(registerDTO)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Act.
+
+	_ = HandleGetSensors(c)
+
+	// Assert.
+	assert.Equal(h.T(), http.StatusOK, rec.Code)
+	var dtos []dto.SensorRegisterDTO
+	_ = json.NewDecoder(rec.Body).Decode(&dtos)
+	assert.Equal(h.T(), 1, len(dtos))
+	afterGetAll := dtos[0]
+	assert.Equal(h.T(), registerDTO.Name, afterGetAll.Name)
+	assert.Equal(h.T(), registerDTO.Uuid, afterGetAll.Uuid)
+	assert.True(h.T(), afterGetAll.ID > 0)
+}
